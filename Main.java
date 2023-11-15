978813f1e6eb32d47ff766a4686542b024227bc9
Joel Varghese Oommen <141248492+Joel-VO@users.noreply.github.com>
2023-11-15 23:08:54 +0530

    Update 2 Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class BinomialDistribution{
     double binomial;
     double []array = new double[1000];
     double fact_n=1,fact_r=1,fact_nr=1;
+    double sum_fair=0,sum_cheat=0;
     BinomialDistribution(int n,double prob){
         for(int r=0;r<=n;r++) {
             //if statements used to prevent division by 0
@@ -29,7 +30,8 @@ class BinomialDistribution{
                     }
                 }
             }
-            binomial = (fact_n/(fact_r*fact_nr))*(Math.pow(prob,r)*Math.pow((1-prob),(n-r)));//heart and soul of the program, the binomial distribution formula
+            binomial = (fact_n/(fact_r*fact_nr))*(Math.pow(prob,r)*Math.pow((1-prob),(n-r)));//heart and soul of the
+            // program, the binomial distribution formula
             array[r] = binomial;
             fact_n=1;
             fact_r=1;
@@ -41,12 +43,25 @@ class BinomialDistribution{
             System.out.println(array[i]);
         }
     }
-    void probability(BinomialDistribution binfair,BinomialDistribution bincheat){
-        //do the slider check here and then return value which is suppose is preferable 
+    int probability(BinomialDistribution binfair,BinomialDistribution bincheat,double fair,double cheater){
+        int flag=0;
+        for(int i=binfair.array.length-1;i>0;i--){
+            sum_fair += binfair.array[i];
+            sum_cheat+=bincheat.array[i];
+            if(sum_fair>fair || sum_cheat>cheater){
+                break;
+            }else if(sum_fair<=fair && sum_cheat<=cheater){
+                //System.out.println("We need to do at least "+i+" out of "+binfair.array.length+" tosses");
+                flag=1;
+                break;
+            }
+        }
+        return flag;
     }
 }
 public class Main {
     public static void main(String[] args) {
+        int flag=0,val = 0;
         BinomialDistribution[] binfair = new BinomialDistribution[1000];
         BinomialDistribution[] bincheat = new BinomialDistribution[1000];
         Scanner sc = new Scanner(System.in);
@@ -58,11 +73,20 @@ public class Main {
         double percentage = sc.nextDouble();
         // do the binfair array declaration, then assign value using method ArrayDeclaration and then do a test to see if
         //array is displayed
-        /*int val = 2;
-        for(int i=0;i<=5;i++){
+        /*int val = 0;
+        for(int f=0;i<=5;i++){
             binfair[i] = new BinomialDistribution(val,0.5);
             val++;
         }
-        binfair[5].Display(8);*/
+        binfair[4].Display(10);*/
+        for(int i=0; flag!=1;i++){
+            binfair[i] = new BinomialDistribution(val,0.5);
+            bincheat[i] = new BinomialDistribution(val,percentage);
+            flag = binfair[i].probability(binfair[i],bincheat[i],fair,cheater);
+            //problem here is that I can't way of terminating the program without
+            //a return value, but that prevents text from being displayed....or is that the case???
+            //checking would be useful
+        }
+
     }
 }
