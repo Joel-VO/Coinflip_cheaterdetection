e1351e1621bd8f0c9c66f633b35b3f97e662f4f0
Joel Varghese Oommen <141248492+Joel-VO@users.noreply.github.com>
2023-11-30 22:29:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,55 +1,51 @@
-import java.util.*;
+import java.math.BigDecimal;
+import java.util.Scanner;
 
 class BinomialDistribution {
-    double[] probabilities; // Array to store the probabilities
+    BigDecimal[] probabilities; // Array to store the probabilities
     int trials; // Number of trials
-    double probability; // Probability of success
+    BigDecimal probability; // Probability of success
 
     // Constructor
     BinomialDistribution(int trials, double probability) {
         this.trials = trials;
-        this.probability = probability;
-        this.probabilities = new double[trials + 1]; // Initialize the probabilities array
+        this.probability = BigDecimal.valueOf(probability);
+        this.probabilities = new BigDecimal[trials + 1]; // Initialize the probabilities array
         calculateProbabilities(); // Calculate the probabilities
     }
 
     // Method to calculate the probabilities
     private void calculateProbabilities() {
         for (int successes = 0; successes <= trials; successes++) {
-            double failureProbability = 1 - probability; // Probability of failure
+            BigDecimal failureProbability = BigDecimal.ONE.subtract(probability); // Probability of failure
             int failures = trials - successes; // Number of failures
-            double combination = calculateCombination(trials, successes); // Calculate the combination
+            BigDecimal combination = calculateCombination(trials, successes); // Calculate the combination
             // Calculate the probability using the binomial distribution formula
-            probabilities[successes] = combination * Math.pow(probability, successes) * Math.pow(failureProbability, failures);
+            probabilities[successes] = combination.multiply(probability.pow(successes)).multiply(failureProbability.pow(failures));
         }
     }
 
     // Method to calculate the combination (nCr)
-    private double calculateCombination(int n, int r) {
-        return factorial(n) / (factorial(r) * factorial(n - r));
-    }
-
-    // Method to calculate the factorial of a number
-    private double factorial(int n) {
-        double result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+    private BigDecimal calculateCombination(int n, int r) {
+        BigDecimal result = BigDecimal.ONE;
+        for (int i = 1; i <= r; i++) {
+            result = result.multiply(BigDecimal.valueOf(n - i + 1)).divide(BigDecimal.valueOf(i));
         }
         return result;
     }
 
     // Method to display the probabilities
     void display() {
-        for (double probability : probabilities) {
+        for (BigDecimal probability : probabilities) {
             System.out.print(" " + probability);
         }
     }
 
     // Method to calculate the sum of probabilities for a given range
-    double sliderValue(int max, int val) {
-        double sum = 0;
+    BigDecimal sliderValue(int max, int val) {
+        BigDecimal sum = BigDecimal.ZERO;
         for (int i = max; i >= val; i--) {
-            sum += probabilities[i];
+            sum = sum.add(probabilities[i]);
         }
         return sum;
     }
@@ -75,11 +71,11 @@ public class Main {
             BinomialDistribution bincheat = new BinomialDistribution(i, percentage);
             for (int j = 0; j <= i; j++) {
                 // Calculate the sum of probabilities for a given range
-                double fair_sum = binfair.sliderValue(i, j);
-                double cheat_sum = bincheat.sliderValue(i, j);
+                BigDecimal fair_sum = binfair.sliderValue(i, j);
+                BigDecimal cheat_sum = bincheat.sliderValue(i, j);
                 // Check if the condition for max fair players falsely accused and minimum cheaters caught is met
-                if (fair_sum <= fair && cheat_sum >= cheater) {
-                    System.out.println("A minimum of " + (j) + " heads out of " + i + " are required to make a decent assumption that someone is cheating.");
+                if (fair_sum.doubleValue() <= fair && cheat_sum.doubleValue() >= cheater) {
+                    System.out.println("A minimum of " + j + " heads out of " + i + " are required to make a decent assumption that someone is cheating.");
                     return;
                 }
             }
